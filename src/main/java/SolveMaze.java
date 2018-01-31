47530d4b5ded096023ec01faf9a9119df4724f46
dkenney1 <dkenney2@illinois.edu>
2018-01-31 14:43:37 -0600

    forked properly, can push now

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,17 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            double random = Math.random();
+            if (random < 0.33) {
+                maze.move();
+            }
+            else if (random < 0.67) {
+                maze.turnLeft();
+                maze.move();
+            } else {
+                maze.turnRight();
+                maze.move();
+            }
         }
 
         if (maze.isFinished()) {
